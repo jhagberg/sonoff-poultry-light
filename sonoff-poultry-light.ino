93ca5472849ce5c6359d7e1a4bd29263fe843b96
Jonas Hagberg <jonas.hagberg@nbis.se>
2018-01-17 14:47:28 +0100

    Clean old webserver things

--- sonoff-poultry-light.ino
+++ sonoff-poultry-light.ino
@@ -86,53 +86,6 @@ void loop()
 }
 
 
-void handleRoot() {
-  char temp[400];
-  int sec = millis() / 1000;
-  int min = sec / 60;
-  int hr = min / 60;
-  time_t tnow = time(nullptr);
-  
-  snprintf ( temp, 400,
-
-             "<html>\
-  <head>\
-    <meta http-equiv='refresh' content='5'/>\
-    <title>Poultry Light ESP</title>\
-    <style>\
-      body { background-color: #cccccc; font-family: Arial, Helvetica, Sans-Serif; Color: #000088; }\
-    </style>\
-  </head>\
-  <body>\
-    <h1>Hello from the poultry!</h1>\
-    <p>Uptime: %02d:%02d:%02d</p>\
-    <p>Time: %s </p>\
-    <p>Heapsize: %02d</p>\
-  </body>\
-</html>",
-
-             hr, min % 60, sec % 60 ,ctime(&tnow)  , ESP.getFreeHeap()
-           );
-  server.send ( 200, "text/html", temp );
-}
-
-void handleNotFound() {
-  String message = "File Not Found\n\n";
-  message += "URI: ";
-  message += server.uri();
-  message += "\nMethod: ";
-  message += ( server.method() == HTTP_GET ) ? "GET" : "POST";
-  message += "\nArguments: ";
-  message += server.args();
-  message += "\n";
-
-  for ( uint8_t i = 0; i < server.args(); i++ ) {
-    message += " " + server.argName ( i ) + ": " + server.arg ( i ) + "\n";
-  }
-
-  server.send ( 404, "text/plain", message );
-
-}
 
 
 // Check for Update
